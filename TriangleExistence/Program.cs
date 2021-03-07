5c23851e596fff8d6de578b01db46d5a4c71c240
Volha Kuralenia <Volha_Kuralenia@epam.com>
2021-03-07 14:17:27 +0300

    1.1

--- TriangleExistence/Program.cs
+++ TriangleExistence/Program.cs
@@ -2,20 +2,22 @@
 
 namespace TriangleExistence
 {
-    class Triangle
+    internal class Triangle
     {
-        public int first, second, third;
+        private int First { get; }
+        private int Second { get; }
+        private int Third { get; }
         public Triangle(int a, int b, int c)
         {
-            first = a;
-            second = b;
-            third = c;
+            First = a;
+            Second = b;
+            Third = c;
         }
         public string TriangleCheck()
         {
-            if ((first + second > third) &&
-                (second + third > first) &&
-                (third + first > second))
+            if ((First + Second > Third) &&
+                (Second + Third > First) &&
+                (Third + First > Second))
             {
                 return "Triangle exists";
             }
@@ -23,39 +25,62 @@ namespace TriangleExistence
         }
         public int TrianglePerimeter()
         {
-            int perimetr = first + second + third;
+            int perimetr = First + Second + Third;
             return perimetr;
         }
         public double GeronSquare()
         {
             double halfP = TrianglePerimeter() / 2;
-            double square = Math.Sqrt(halfP * (halfP - first) *
-                                     (halfP - second) * (halfP - third));
-            return Math.Round(square, 2);
+            double square = Math.Sqrt(halfP * (halfP - First) *
+                                     (halfP - Second) * (halfP - Third));
+            return square;
         }
     }
     class Program
     {
-        private static Triangle EnterTriangleSides()
+        private static string EnterTriangleSides()
         {
-            Console.WriteLine();
             Console.WriteLine("Enter triangle sides: ");
-            string line = Console.ReadLine();
-            string[] numbersString = line.Split(',');
-            return new Triangle(int.Parse(numbersString[0]),
-                                int.Parse(numbersString[1]),
-                                int.Parse(numbersString[2]));
+            return Console.ReadLine();
         }
-        private static void OutPut(Triangle myTriangle)
+        private static int[] ParseLine(string line)
         {
+            try
+            {
+                string[] numbersString = line.Split(',');
+                int[] numbersInt = Array.ConvertAll(numbersString, int.Parse);
+                return numbersInt;
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return null;
+        }
+        private static void OutPut(int[] numbers)
+        {
+            Triangle myTriangle = new Triangle(numbers[0], numbers[1], numbers[2]);
+            Console.WriteLine();
             Console.WriteLine(myTriangle.TriangleCheck());
             Console.WriteLine($"Perimeter is: {myTriangle.TrianglePerimeter()}");
-            Console.WriteLine($"Square is: {myTriangle.GeronSquare()}");
+            Console.WriteLine($"Square is: {myTriangle.GeronSquare():F2}");
         }
         static void Main()
         {
-            Triangle myTriangle = EnterTriangleSides();
-            OutPut(myTriangle);
+            string line = EnterTriangleSides();
+            int[] numbers = ParseLine(line);
+            if (numbers.Length == 0)
+            {
+                Console.WriteLine("You didn't enter numbers");
+            }
+            else if (numbers.Length != 3)
+            {
+                Console.WriteLine("Enter three numbers");
+            }
+            else
+            {
+                OutPut(numbers);
+            }
         }
     }
 }
